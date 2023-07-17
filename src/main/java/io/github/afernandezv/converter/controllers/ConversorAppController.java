4847cabc7b251499f11f7cf387e65480240561e8
Alberto Fernández <a.fernandezv09@gmail.com>
2023-07-17 16:27:47 -0600

    App controller added

---
+++ src/main/java/io/github/afernandezv/converter/controllers/ConversorAppController.java
@@ -0,0 +1,14 @@
+package io.github.afernandezv.converter.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConversorAppController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
