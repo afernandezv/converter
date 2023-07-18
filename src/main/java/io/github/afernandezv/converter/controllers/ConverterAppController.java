588656cc35f5debc44b69a0a94a7c46e2a31039b
Alberto Fernández <a.fernandezv09@gmail.com>
2023-07-17 21:27:03 -0600

    Methods for initialize controls added

--- src/main/java/io/github/afernandezv/converter/controllers/ConverterAppController.java
+++ src/main/java/io/github/afernandezv/converter/controllers/ConverterAppController.java
@@ -1,5 +1,8 @@
 package io.github.afernandezv.converter.controllers;
 
+import io.github.afernandezv.converter.enums.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -8,6 +11,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ConverterAppController<T> implements Initializable {
@@ -43,8 +47,33 @@ public class ConverterAppController<T> implements Initializable {
     @FXML
     private Label resultText;
 
+    private Category category;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    private void initControls(){
+        setHeaderText();
+        populateChoiceBox();
+    }
+
+    private void setHeaderText(){
+        headerText.setText("Conversor de " + this.category.getDescription());
+    }
+
+    private void populateChoiceBox(){
+        List<T> itemsList = switch (this.category.name()) {
+            case "CURRENCY" -> CurrencyType.getTypes();
+            case "LENGTH" -> LengthUnit.getUnits();
+            case "MASS" -> MassUnit.getUnits();
+            default -> TemperatureUnit.getUnits();
+        };
+        ObservableList<T> items = FXCollections.observableList(itemsList);
+        fromSelect.setItems(items);
+        toSelect.setItems(items);
+        fromSelect.getSelectionModel().selectFirst();
+        toSelect.getSelectionModel().select(1);
     }
 }
