646ea344ad067ba47064ba549ec70a550c74d087
Alberto Fernández <a.fernandezv09@gmail.com>
2023-07-17 18:55:03 -0600

    Modules updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module io.github.afernandezv.converter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires com.google.gson;
+    requires java.dotenv;
 
 
     opens io.github.afernandezv.converter to javafx.fxml;
     exports io.github.afernandezv.converter;
     exports io.github.afernandezv.converter.controllers;
     opens io.github.afernandezv.converter.controllers to javafx.fxml;
+    opens io.github.afernandezv.converter.pojo.currencyconversion to com.google.gson;
+    opens io.github.afernandezv.converter.pojo.unitconversion to com.google.gson;
 }
