7027ec4d63ba3718332e094e9aa85e8112155353
Alberto Fernández <a.fernandezv09@gmail.com>
2023-07-17 19:36:10 -0600

    FXML components added

--- src/main/java/io/github/afernandezv/converter/controllers/ConverterAppController.java
+++ src/main/java/io/github/afernandezv/converter/controllers/ConverterAppController.java
@@ -1,11 +1,48 @@
 package io.github.afernandezv.converter.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.kordamp.ikonli.javafx.Icon;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ConverterAppController implements Initializable {
+public class ConverterAppController<T> implements Initializable {
+    // Menu Icons
+    @FXML
+    private Icon currencyButton;
+
+    @FXML
+    private Icon lengthButton;
+
+    @FXML
+    private Icon massButton;
+
+    @FXML
+    private Icon temperatureButton;
+
+    // Controls
+    @FXML
+    private TextField valueText;
+
+    @FXML
+    private ChoiceBox<T> fromSelect;
+
+    @FXML
+    private ChoiceBox<T> toSelect;
+
+    @FXML
+    private Button convertButton;
+
+    @FXML
+    private Label headerText;
+
+    @FXML
+    private Label resultText;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
