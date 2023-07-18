91a998ed054edeb611bd023a313eaaad0787a113
Alberto Fernández <a.fernandezv09@gmail.com>
2023-07-18 12:49:40 -0600

    Reformat code

--- src/main/java/io/github/afernandezv/converter/utils/UnitConverter.java
+++ src/main/java/io/github/afernandezv/converter/utils/UnitConverter.java
@@ -12,17 +12,17 @@ import java.net.http.HttpResponse;
 
 public class UnitConverter {
 
-    public static double convertUnit(String type, double value, String fromUnit, String toUnit){
+    public static double convertUnit(String type, double value, String fromUnit, String toUnit) {
         return getConversion(type, value, fromUnit, toUnit);
     }
 
-    private static double getConversion(String type, double value, String fromUnit, String toUnit){
+    private static double getConversion(String type, double value, String fromUnit, String toUnit) {
         Dotenv dotenv = Dotenv.load();
         Gson gson = new Gson();
         UnitConversion unitConversion;
 
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://measurement-unit-converter.p.rapidapi.com/"+type+"?value="+value+"&from="+fromUnit+"&to="+toUnit))
+                .uri(URI.create("https://measurement-unit-converter.p.rapidapi.com/" + type + "?value=" + value + "&from=" + fromUnit + "&to=" + toUnit))
                 .header("X-RapidAPI-Key", dotenv.get("RAPID_API_KEY"))
                 .header("X-RapidAPI-Host", dotenv.get("RAPID_API_HOST"))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -31,7 +31,7 @@ public class UnitConverter {
         try {
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             unitConversion = gson.fromJson(response.body(), UnitConversion.class);
-        }catch (IOException | InterruptedException e){
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException();
         }
         return unitConversion.getValue();
